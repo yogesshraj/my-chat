81395d1f14b337219fe872e79646239003435012
yogesshraj <yogessh19@navgurukul.org>
2025-12-05 12:30:23 +0530

    chanjges

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ['https://myakka.qzz.io', 'http://localhost:3000'],
     methods: ["GET", "POST"]
   }
 });
